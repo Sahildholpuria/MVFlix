b0c744cc58820154aaa985d255e14bd0f5a0e9ad
sahil dholpuria <dholpuria1999@gmail.com>
2021-05-25 22:39:42 +0530

    update

--- app/src/main/java/com/example/demo/MainActivity2.java
+++ app/src/main/java/com/example/demo/MainActivity2.java
@@ -8,16 +8,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.khizar1556.mkvideoplayer.MKPlayerActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    MenuItem menuItem;
+
+    private String url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        playButton = findViewById(R.id.button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MKPlayerActivity.configPlayer(MainActivity2.this).play(url);
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
