fd49efe22255e9b9aa00f6126c51c237d9620e07
sahil dholpuria <dholpuria1999@gmail.com>
2021-06-08 23:03:15 +0530

    update

--- app/src/main/java/com/example/demo/MainActivity4.java
+++ app/src/main/java/com/example/demo/MainActivity4.java
@@ -12,7 +12,21 @@ public class MainActivity4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
+
+        String summary = "<html><body><div class=\"chat\">\n" +
+                "      <iframe\n" +
+                "      src='https://webchat.botframework.com/embed/propro12?s=xGfRhE0Igu4.RNwNy8meJ4XLSxg2RHbUuP32QKF06wyuU482hhz9XYc' \n" +
+                "        style=\"\n" +
+                "        margin-left: 25%;\n" +
+                "        margin-top: 8%;\n" +
+                "          min-width: 300px;\n" +
+                "          width: 50%;\n" +
+                "          min-height: 500px;\"\n" +
+                "      ></iframe>\n" +
+                "    </div></body></html>";
+
         WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("https://sahildholpuria.tech");
+//        webView.loadUrl("https://sahildholpuria.tech");
+        webView.loadData(summary, "text/html", null);
     }
 }
